582302b6e0665310564d0ea0b3471a8803592346
juaasca <juanka31998@gmail.com>
2019-09-24 19:09:59 +0200

    login

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+import { environment } from '../../environments/environment';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+    auth: any;
+    database: any;
+    provider: any;
+
+    constructor() { }
+
+    ngOnInit() {
+        // Initialize Firebase
+        firebase.initializeApp(environment.firebaseConfig);
+        this.database = firebase.database();
+        this.auth = firebase.auth();
+        this.auth.languageCode = 'es';
+        this.provider = new firebase.auth.GoogleAuthProvider();
+
+        this.auth.signInWithPopup(this.provider)
+            .then((result: any) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const token = result.credential.accessToken;
+                console.log(token);
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error: any) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                const credential = error.credential;
+            });
+    }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyBYZlTRljNLNEHq0z7SG8IBgxnmGcAfZz4',
+        authDomain: 'pay2park-fbf82.firebaseapp.com',
+        databaseURL: 'https://pay2park-fbf82.firebaseio.com',
+        projectId: 'pay2park-fbf82',
+        storageBucket: 'pay2park-fbf82.appspot.com',
+        messagingSenderId: '474680165958',
+        appId: '1:474680165958:web:322e54ad321fd9353d2c47'
+    }
 };
 
 /*
