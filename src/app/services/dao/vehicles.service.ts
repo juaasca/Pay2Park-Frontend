da84e9de84393fb1728c597d37e93ea55bdbb93f
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-02 19:29:40 +0200

    Creada clase servicios para la persistencia de los vehículos.

---
+++ src/app/services/dao/vehicles.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { Vehicle } from 'src/app/Domain/Vehicle';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class VehiclesService {
+  private ref: firebase.database.Reference;
+  private refVehicles: firebase.database.Reference;
+  private vehiclesDataBaseUrl: string = 'vehicles/cars';
+
+  constructor() {
+      this.initializeDatabase();
+  }
+
+  private initializeDatabase() {
+      this.ref = firebase.app().database().ref();
+      this.refVehicles = this.ref.child(this.vehiclesDataBaseUrl);
+  }
+
+  public async showDatabase() {
+      let snapshot = await this.refVehicles.once('value');
+      return snapshot.val();
+  }
+
+  public addVehicle(vehicle: Vehicle) {
+      this.refVehicles.child(vehicle.LicensePlate).set(vehicle);
+  }
+
+  public getVehicle(licensePlate: string) {
+      return this.refVehicles.child(licensePlate).once('value').then(function(snapshot) {
+        var vehicleName = snapshot.val() && snapshot.val().Name;
+        var vehicleDescription = snapshot.val() && snapshot.val().Description;
+        
+        return new Vehicle(licensePlate, vehicleName, vehicleDescription);
+      });
+
+  }
+
+  public deleteVehicle(licensePlate: string) {
+      this.refVehicles.child(licensePlate).remove();
+  }
+}
