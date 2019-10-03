807252ec1c5a13ec59b348548b6f68c7f5ceae3b
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-04 00:10:13 +0200

    Implementado constructor de la clase worker.

--- src/app/Domain/Worker.ts
+++ src/app/Domain/Worker.ts
@@ -1,5 +1,8 @@
 import { Person } from './Person';
 
 export class Worker extends Person {
-    constructor()
+    constructor(dni: string, name: string, surname: string, username: string, birthDate: Date, mail: string)
+    {
+        super(dni, name, surname, username, birthDate, mail);
+    }
 }
