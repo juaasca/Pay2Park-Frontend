26697ebc4b26defcef738fe77531fb047fb3b3ec
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-07 00:09:07 +0200

    Añadido manejo de errores en el caso de registrar un nuevo usuario.

--- src/app/logic/user.actions.service.ts
+++ src/app/logic/user.actions.service.ts
@@ -12,15 +12,15 @@ export class UserActions {
     }
     
     public registerNewUser(dni: string, name: string, surname: string, username: string, password: string, birthDate: Date, email: string) {
-        var newClient =
-          new Client(dni, name, surname, username, birthDate, email);
-    
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-          var errorMessage = error.message;
-          
-          throw new Error(errorMessage);
-        });
-    
-        this.clientsService.addClient(newClient);
-      }
+      var newClient =
+        new Client(dni, name, surname, username, birthDate, email);
+
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        this.clientsService.addEntity(newClient.Username, newClient);
+      }).catch(function(error) {
+        var errorMessage = error.message;
+        
+        throw new Error(errorMessage);
+      });
+    }
 }
