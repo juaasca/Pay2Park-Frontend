30b51a051d6513f3c90e54ef3dc6bb8cf9328a7e
MrJavierCM <jacamia@inf.upv.es>
2019-10-07 22:25:15 +0200

    Validaciones del formulario de registro

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Location } from '@angular/common';
 import * as firebase from 'firebase';
 import { Client } from 'src/app/Domain/Client';
 import { ClientsService } from 'src/app/services/dao/clients.service';
+import {passwordValidation} from './passwordValidation';
+import {usernameValidation} from './usernameValidation';
 
 @Component({
   selector: 'app-registration',
@@ -16,16 +18,33 @@ export class RegistrationComponent implements OnInit {
 
   constructor(private location: Location, private formBuilder: FormBuilder, private clientsService : ClientsService) {
     this.registration = this.formBuilder.group({
-      Nombre: ['',Validators.required],
-      Apellidos: ['', Validators.required],
-      Usuario: ['', Validators.required],
-      RetryPassword: this.formBuilder.group({
-        Password: ['',Validators.required],
-        ConfirmPassword: ['', Validators.required],
+      Name: ['', Validators.required],
+      Surname: ['', Validators.required],
+      Username: new FormControl('', Validators.compose([
+        Validators.required,
+        usernameValidation.validUsername,
+        Validators.minLength(8)
+      ])),
+      RetryPassword: new FormGroup({
+         Password: new FormControl ('', Validators.compose([
+            Validators.required,
+            Validators.minLength(8), 
+            Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+         ])),
+         ConfirmPassword: new FormControl('', Validators.required)
+      //RetryPassword: this.formBuilder.group({
+        //Password: ['', Validators.required, Validators.minLength(8), 
+        //        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')],
+        //ConfirmPassword: ['', Validators.required, Validators.minLength(8)],
+      }, (formGroup: FormGroup) => {
+        return passwordValidation.areEqual(formGroup);
       }),
-      FechaNacimiento: ['', Validators.required],
-      Email: ['', Validators.required],
-      DNI: ['', Validators.required],
+      Birthdate: ['', Validators.required],
+      Email: new FormControl ('', Validators.compose([
+        Validators.required, 
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      DNI: ['', Validators.required, Validators.minLength(9)],
     });
   }
 
@@ -35,7 +54,7 @@ export class RegistrationComponent implements OnInit {
     var formValue = this.registration.value;
 
     var newClient =
-      new Client(formValue.DNI, formValue.Nombre, formValue.Apellidos, formValue.Usuario, formValue.FechaNacimiento, formValue.Email);
+      new Client(formValue.DNI, formValue.Name, formValue.Surname, formValue.Username, formValue.Birthdate, formValue.Email);
 
     firebase.auth().createUserWithEmailAndPassword(newClient.Mail, formValue.RetryPassword.Password);
 
@@ -44,5 +63,37 @@ export class RegistrationComponent implements OnInit {
 
   volver(){
     this.location.back();
+  }
+
+  validation_messages = {
+    'Name': [
+          {type: 'required', message: 'El nombre es necesario.'}
+    ],
+    'Surname': [
+          {type: 'required', message: 'El apellido es necesario.'}
+    ],
+    'Username': [
+          {type: 'required', message: 'El nombre de usuario es necesario.'},
+          {type: 'minLength', message: 'La longitud mínima es de 8 caracteres.'},
+          {type: 'validUsername', message: 'El nombre de usuario ya está cogido.'}
+    ],
+    'RetryPassword': [
+      {type: 'required', message: 'La contraseña es necesaria.'},
+      {type: 'minLength', message: 'La longitud mínima es de 8 caracteres.'},
+      {type: 'pattern', message: 'La contraseña debe contener mínimo una letra minúscula, una letra mayúscula y un número. No puede contener caracteres especiales.'}
+    ],
+    'Birthdate': [
+      {type: 'required', message: 'La fecha de nacimiento es necesario.'}
+      
+    ],
+    'Email': [
+      {type: 'required', message: 'El correo electrónico es necesario.'},
+      {type: 'pattern', message: 'Correo incorrecto.'}
+      
+    ],
+    'DNI': [
+      {type: 'required', message: 'El DNI es necesario.'}
+      
+    ],
   }
 }
