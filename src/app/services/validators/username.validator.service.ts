68c9c84730689c92ad64be25e185f28b50bdf227
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-08 19:08:13 +0200

    Eliminado validator del usuario y creado otro diferente.

---
+++ src/app/services/validators/username.validator.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ClientsService } from '../dao/clients.service';
+import { Client } from 'src/app/Domain/Client';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsernameValidatorService {
+  static ExistingUsers: Client[] = [];
+
+  constructor(private clientService: ClientsService) {
+    this.clientService.getEntitiesAsync().then((users) => UsernameValidatorService.ExistingUsers = users);
+  }
+
+  static validUsername (fc: FormControl){
+    var userExists = UsernameValidatorService.ExistingUsers.some(user => {
+      return fc.value.toLowerCase() === user.Username.toLowerCase();
+    });
+    
+    if (!userExists) {
+      return (null);
+    } else {
+      return ({validUsername: true});
+    }
+  }
+}
