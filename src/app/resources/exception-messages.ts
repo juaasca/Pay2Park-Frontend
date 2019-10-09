b28cfba3362e0ee65c62ff7020a6d784a7250848
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-09 18:59:02 +0200

    Creada clase para alojar los mensajes de excepciones y así tenerlos localizados.

---
+++ src/app/resources/exception-messages.ts
@@ -0,0 +1,6 @@
+export class ExceptionMessages {
+    static userAlreadyInUse = 'Usuario en uso.';
+    static emailAlreadyInUse = 'Ya existe una cuenta con este usuario.';
+    static errorCreatingUser = 'Ha ocurrido un error intentando crear el usuario.';
+    static invalidEmail = 'El correo electrónico introducido no es correcto.';
+}
