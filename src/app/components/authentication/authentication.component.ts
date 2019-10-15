89ded3f3586e49c359222edb0fc87569c414eaf3
juaasca <juanka31998@gmail.com>
2019-10-15 19:46:48 +0200

    Crear usuario si se registra por google

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import * as firebase from 'firebase';
 
-import { ClientsService } from '../../services/dao/clients.service';
 import { UserActions } from 'src/app/logic/user.actions.service';
 
 @Component({
@@ -13,12 +11,9 @@ import { UserActions } from 'src/app/logic/user.actions.service';
 })
 export class AuthenticationComponent implements OnInit {
 
-    auth: any;
-    provider: any;
     public loginForm: FormGroup;
 
     constructor(private router: Router,
-        private clientService: ClientsService,
         private userActions: UserActions,
         private formBuilder: FormBuilder) {
 
@@ -38,36 +33,17 @@ export class AuthenticationComponent implements OnInit {
     ngOnInit() { }
 
     logWithGoogle() {
-        this.auth = firebase.auth();
-        this.auth.languageCode = 'es';
-        this.provider = new firebase.auth.GoogleAuthProvider();
-
-        this.auth.signInWithPopup(this.provider)
-            .then((result: any) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const token = result.credential.accessToken;
-                console.log(token);
-                // The signed-in user info.
-                const user = result.user;
-                console.log(user);
-                this.router.navigateByUrl('tabs/park');
-            })
-            .catch((error: any) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                const credential = error.credential;
-            });
+        this.userActions.signinUserAsync();
     }
+
     register() {
         this.router.navigateByUrl('registration')
     }
+
     forgot() {
         this.router.navigateByUrl('forgot')
     }
+
     logIn() {
         let formValue = this.loginForm.value;
 
