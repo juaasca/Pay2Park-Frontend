607b3095722ff1fc5e19fb7eaad08c06194c0a1c
Aaron <aaronleonramon@gmail.com>
2019-10-16 16:28:33 +0200

    Añadido icono posicion actual

--- src/app/components/park/park.component.ts
+++ src/app/components/park/park.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { icon, latLng, marker, polyline, tileLayer } from 'leaflet';
+
+
+
 declare let L;
 
 @Component({
@@ -11,10 +15,24 @@ export class ParkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const map = L.map('map').setView([39.47,-0.39], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const map = L.map('map').setView([39.482,-0.348], 16);
+        
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+    
+    let posicion = marker([39.482395, -0.348349], {
+      icon: icon({
+        iconSize: [ 25, 41 ],
+        iconAnchor: [ 13, 41 ],
+        iconUrl: 'assets/leaflet/images/marker-icon.png',
+        shadowUrl: 'assets/leaflet/images/marker-shadow.png'
+      })
+    }).addTo(map);
+    
+    
+
+
   }
 
 }
