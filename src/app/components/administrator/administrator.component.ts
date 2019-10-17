f387afe8cbf6e1d1dc1fce34811a7fb75cdeecd9
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-18 00:04:42 +0200

    Implementado componente del administrador.

--- src/app/components/administrator/administrator.component.ts
+++ src/app/components/administrator/administrator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-administrator',
@@ -7,13 +8,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./administrator.component.scss'],
 })
 export class AdministratorComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private mainComponent: MainComponent) { }
 
   ngOnInit() {}
 
   manageClients(){
-    this.router.navigateByUrl('manage-clients');
+    this.router.navigateByUrl('main/admin/manage-clients');
+  }
+
+  changeToClientView() {
+    this.mainComponent.AdminWindow = false;
+    this.router.navigateByUrl('main/park');
   }
 
 }
