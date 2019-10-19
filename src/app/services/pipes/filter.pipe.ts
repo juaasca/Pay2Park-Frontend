19c1fc77de6996cd05e96038c327e477b749bc66
Carlos Zamora Zamora <carzaza@outlook.es>
2019-10-19 11:25:24 +0200

    Añadido salto de línea.

--- src/app/services/pipes/filter.pipe.ts
+++ src/app/services/pipes/filter.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'filter'
 })
+
 export class FilterPipe implements PipeTransform {
   transform ( array: any[], text: string, column: string): any[] {
     console.log(text);
