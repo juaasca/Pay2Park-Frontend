c553b0a114c0ace429561b61de07780d3e257319
juaasca <juanka31998@gmail.com>
2019-10-22 17:20:17 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBYZlTRljNLNEHq0z7SG8IBgxnmGcAfZz4',
+    authDomain: 'pay2park-fbf82.firebaseapp.com',
+    databaseURL: 'https://pay2park-fbf82.firebaseio.com',
+    projectId: 'pay2park-fbf82',
+    storageBucket: 'pay2park-fbf82.appspot.com',
+    messagingSenderId: '474680165958',
+    appId: '1:474680165958:web:322e54ad321fd9353d2c47'
+  }
 };
