7fce46ec2e47f5340c0905e8347296e1717e98d3
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-04 18:54:30 +0100

    Implementada función que envía un correo de bienvenida a los nuevos revisores.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as nodemailer from 'nodemailer';
+import Mail = require('nodemailer/lib/mailer');
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
@@ -22,3 +24,38 @@ export const deleteUser = functions.https.onCall((request) => {
             throw new functions.https.HttpsError(error.code, error.message);
         });
 });
+
+export const sendWorkerWelcomeEmail = functions.https.onCall(async (request) => {
+    const email = request.Email;
+    const displayName = request.Name;
+    const password = request.Password;
+    
+    var firebaseEmail = functions.config().gmail.email;
+    var firebaseEmailPassword = functions.config().gmail.pass;
+
+    const mailTransport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: firebaseEmail,
+          pass: firebaseEmailPassword,
+        },
+    });
+  
+    const mailOptions: Mail.Options = {
+      from: `Pay2Park <noreply@firebase.com>`,
+      to: email,
+    };
+    
+    // The user subscribed to the newsletter.
+    mailOptions.subject = `¡Bienvenido a Pay2Park!`;
+    let emailText = `¡Hola ${displayName || ''}!`;
+    emailText += `\n\nHas sido dado de alta como revisor en la aplicación Pay2Park. Puedes entrar en la aplicación con los siguientes datos:`;
+    emailText += `\n\nCorreo: ${email}\nContraseña: ${password}\n\n`;
+    emailText += `Te recomendamos que cambies esta contraseña lo antes posible.`;
+    emailText += `\n\nSaludos,\nEl equipo de Pay2Park`;
+    mailOptions.text = emailText;
+    
+    await mailTransport.sendMail(mailOptions);
+  
+    return null;
+  });
