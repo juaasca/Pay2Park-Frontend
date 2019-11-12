4da526f38f14ef376a1c05c44fe3fc4968a008ae
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-12 19:04:00 +0100

    Añadido servicio para registrar la creación de una tarifa.

---
+++ src/app/logic/tariff.actions.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { TariffService } from '../services/dao/tariff.service';
+import { Tariff } from '../Domain/Tariff';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TariffActionsService {
+  constructor(private tariffService: TariffService) { }
+
+  registerNewTariffAsync(tariff: Tariff) {
+    return this.tariffService.addEntityAsync(undefined, tariff);
+  }
+}
