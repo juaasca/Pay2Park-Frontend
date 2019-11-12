d95589fc878eef4f76b4d24af24d5181bbdff004
MrJavierCM <jacamia@inf.upv.es>
2019-11-12 19:34:53 +0100

    Tarifa fake para probar que funciona la lista

--- src/app/components/tariff/manage-tariff/manage-tariff.component.ts
+++ src/app/components/tariff/manage-tariff/manage-tariff.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TariffService } from 'src/app/services/dao/tariff.service';
+import { Tariff } from 'src/app/Domain/Tariff';
 
 @Component({
   selector: 'app-manage-tariff',
@@ -7,9 +9,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./manage-tariff.component.scss'],
 })
 export class ManageTariffComponent implements OnInit {
+  private tariffs: Tariff[] = [];
+  private tariff: Tariff = null;
 
-  constructor(private router: Router) { }
+  constructor(private tariffService: TariffService, private router: Router) { 
+    /* this.updateTariffs(); */
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.tariff = new Tariff(false, "PRUEBA", 20, 120);
+    this.tariffs[0] = this.tariff;
+  }
 
+  updateTariffs(){
+    this.tariffService.getEntitiesAsync().then((tariffs) => this.tariffs = tariffs.sort((a, b) => this.sortNameAscending(a, b)));
+  }
+
+  sortNameAscending(tariffA: Tariff, tariffB: Tariff) {
+    var nameA=tariffA.Price, nameB=tariffB.Price
+    if (nameA < nameB)
+        return -1 
+    if (nameA > nameB)
+        return 1
+    return 0
+  }
 }
