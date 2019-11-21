0ff7a4ed4716c9f848e9a6bf092fbe0342af4fa6
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-21 18:29:24 +0100

    Añadida propiedad para indicar si el bono es multicoche o no.

--- src/app/Domain/Subscription.ts
+++ src/app/Domain/Subscription.ts
@@ -5,15 +5,17 @@ export class Subscription {
     Name: string;
     durationInMilliseconds: Number;
     Price: Number;
+    IsMultiCar: boolean;
 
-    constructor(name: string, durationInMilliseconds: Number, price: Number) {
-        this.Identifier = Subscription.getIdentifier(name, durationInMilliseconds, price);
+    constructor(name: string, durationInMilliseconds: Number, price: Number, isMultiCar: boolean) {
+        this.Identifier = Subscription.getIdentifier(name, durationInMilliseconds, price, isMultiCar);
         this.Name = name;
         this.durationInMilliseconds = durationInMilliseconds;
         this.Price = price;
+        this.IsMultiCar = isMultiCar;
     }
 
-    public static getIdentifier(name: string, durationInMilliseconds: Number, price: Number) {
-        return StringUtils.getHashCode(`${name}${durationInMilliseconds}${price}`);
+    public static getIdentifier(name: string, durationInMilliseconds: Number, price: Number, isMultiCar: boolean) {
+        return StringUtils.getHashCode(`${name}${durationInMilliseconds}${price}${isMultiCar}`);
     }
 }
